f029bf1ac59c43d8931b886cdd3fd4ffa7e6cb78
VkasSharma <vikassharma10101995@gmail.com>
2025-01-01 12:57:33 +0530

    change bucket name

--- c2-s3.tf
+++ c2-s3.tf
@@ -2,7 +2,7 @@ module "website_s3_bucket" {
   source  = "app.terraform.io/vikas-demo-1/s3-website/aws"
   version = "1.0.1"
   # insert required variables here
-  bucket_name = "cassie_demo-sample-buckte123456712345"
+  bucket_name = "cassiedemo-sample-buckte123456712345"
   tags = {
 	env = "dev"
 	}

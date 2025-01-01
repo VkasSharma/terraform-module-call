d9a2e6f02152b1a3908262035445abba83cf6d7e
VkasSharma <47387771+VkasSharma@users.noreply.github.com>
2025-01-01 12:46:18 +0530

    Update c2-s3.tf

--- c2-s3.tf
+++ c2-s3.tf
@@ -2,7 +2,7 @@ module "s3-website" {
   source  = "app.terraform.io/vikas-demo-1/s3-website/aws"
   version = "1.0.1"
   # insert required variables here
-  bucket_name = "cassie_demo-sample-buckte"
+  bucket_name = "cassie_demo-sample-buckte123456712345"
   tags = {
 	env = "dev"
 	}
